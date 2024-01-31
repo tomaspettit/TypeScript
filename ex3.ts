577b82c08c87577f67402e52011748defc2b0693
TOMAS PETTIT - STUDENT <g00419414@atu.ie>
2024-01-31 11:30:17 +0000

    Added ex3.ts

---
+++ ex3.ts
@@ -0,0 +1,7 @@
+function addition(value: string) {
+    console.log("Value is: " + value);
+    }
+    let firstVal: number = 42;
+    let secondVal: number = 1;
+    let sumOfVals: string = (firstVal + secondVal).toLocaleString();
+    addition(sumOfVals);
